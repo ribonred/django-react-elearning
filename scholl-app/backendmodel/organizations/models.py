3d58ff8e4e14da0b68eba0842e8f6ac606ac37bc
rede akbar <ribonred@gmail.com>
2020-04-09 00:13:10 +0700

    relation

--- scholl-app/backendmodel/organizations/models.py
+++ scholl-app/backendmodel/organizations/models.py
@@ -3,15 +3,6 @@ from backendmodel.core.models import BaseTimeStampModel, User
 import uuid
 
 
-class CompanyDivision(BaseTimeStampModel):
-    division_name = models.CharField(max_length=255)
-    division_member = models.ForeignKey(
-        User, on_delete=models.DO_NOTHING, related_name='member_division', null=True, blank=True)
-
-    def __str__(self):
-        return self.division_name
-
-
 class Company(BaseTimeStampModel):
     company_manager = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='owner_company')
@@ -19,8 +10,25 @@ class Company(BaseTimeStampModel):
     company_address = models.TextField()
     company_code = models.UUIDField(
         default=uuid.uuid4, editable=False, unique=True)
-    company_division = models.ForeignKey(
-        CompanyDivision, null=True, blank=True, related_name='division', on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.company_name
+
+
+class CompanyDivision(BaseTimeStampModel):
+    company_division = models.ForeignKey(
+        Company, null=True, blank=True, related_name='division', on_delete=models.CASCADE)
+    division_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.division_name
+
+
+class DivisionMember(BaseTimeStampModel):
+    division = models.ForeignKey(
+        CompanyDivision, on_delete=models.CASCADE, related_name='company_member_division', null=True, blank=True)
+    division_member = models.ForeignKey(
+        User, on_delete=models.DO_NOTHING, related_name='member', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.division} || {self.division_member.fullname}'
