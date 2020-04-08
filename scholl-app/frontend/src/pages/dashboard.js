03cc6e6314affd8d4daa8fa883671d1f02527dfb
agamw11 <agam.wardani@riseandshine-expo.com>
2020-04-09 01:43:50 +0800

    apply formgroup to aignup

--- scholl-app/frontend/src/pages/dashboard.js
+++ scholl-app/frontend/src/pages/dashboard.js
@@ -1,9 +1,71 @@
 import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  UsergroupAddOutlined
+} from '@ant-design/icons';
 
-export default class Dashboard extends Component {
-    render() {
-        return (
-            <h1> hallo login sukes</h1>
-        )
-    }
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+class Dashboard extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <UserOutlined />
+              <span>User Profile</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <DesktopOutlined />
+              <span>Company Profile</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <TeamOutlined />
+                  <span>Member</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Teacher</Menu.Item>
+              <Menu.Item key="4">Student</Menu.Item>
+              <Menu.Item key="5">Officer</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
 }
+
+export default Dashboard
