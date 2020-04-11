5f2315927d33dd72cf1e2da3959db85c8e7f9bfc
rede akbar <ribonred@gmail.com>
2020-04-11 20:45:32 +0700

    api func

--- scholl-app/frontend/src/pages/signuppages.js
+++ scholl-app/frontend/src/pages/signuppages.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import SignupView from '../component/signup'
-import { request } from '../helper/requestHelper'
+import { request, get_token } from '../helper/requestHelper'
+import { registeruser } from '../reduxActions/dashboard'
+import { connect } from 'react-redux'
 
 class SignupPage extends React.Component {
     state = {
         form: {
-          role: 3,
-          jenis_kelamin: "laki-laki"
+            role: 3,
+            jenis_kelamin: "laki-laki"
         },
     }
 
     onFormChange = (fieldName, e) => {
-        const formObj = {...this.state.form};
-        if(!e.target){
+        const formObj = { ...this.state.form };
+        if (!e.target) {
             formObj[fieldName] = e
             this.setState({
                 form: formObj,
@@ -22,20 +24,22 @@ class SignupPage extends React.Component {
             this.setState({
                 form: formObj,
             })
-        }  
+        }
     }
 
-    onsubmit = async() => {
-        const result = await request('/api/users/', {
-            method: 'POST',
-           }, this.state.form);
-        if(result){
-            localStorage.setItem('token', result.data.token)
-        }       
+    onsubmit = async () => {
+        this.props.dispatch(registeruser(get_token(), this.state.form))
+        // const result = await request('/api/users/', {
+        //     method: 'POST',
+        // }, this.state.form);
+        // if (result) {
+        //     localStorage.setItem('token', result.data.token)
+        // }
     }
     render() {
         return <SignupView userform='test' onFormChange={this.onFormChange} onsubmit={this.onsubmit} />
     }
 }
 
-export default SignupPage
+
+export default connect()(SignupPage)
