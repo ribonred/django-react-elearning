56ba6fb34156d0b21b7c20a0adde932b3bdbf867
rede akbar <ribonred@gmail.com>
2020-04-11 15:49:50 +0700

    bearer token

--- scholl-app/backendmodel/account/urls.py
+++ scholl-app/backendmodel/account/urls.py
@@ -1,7 +1,8 @@
-from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token as tokenview
+from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token as tokenview, verify_jwt_token
 from django.urls import path, include
 
 urlpatterns = [
     path('token-auth/', tokenview),
     path('token-refresh/', refresh_jwt_token),
+    path('token-verify/', verify_jwt_token),
 ]
