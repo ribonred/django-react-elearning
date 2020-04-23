d8fa828f6c384d0a2c96cdf144175a00c2145d54
ramadhan.fiki2 <ramdhan.fiki2@gmail.com>
2020-04-23 09:04:40 +0700

    copy brach agarez ke frontEndNew

--- scholl-app/frontend/src/component/lknview.js
+++ scholl-app/frontend/src/component/lknview.js
@@ -1,25 +1,39 @@
 import React from 'react'
 import ViewContainer from '../ui-container/viewContainer';
-import { Link } from 'react-router-dom'
-
-import { Descriptions, Button } from 'antd';
+import { Descriptions } from 'antd';
 
 
 const LknViewView = (props) => {
-    let noLkn=''
-    let tanggal_dibuat='';
+    let noLkn          = ''
+    let tanggal_dibuat = ''
+    let noPenangkapan  = ''
+    let tglPenangkapan = ''
+    let jamPenangkapan = ''
+
     if(!props.lkn[0]){
-        noLkn = '';
-        tanggal_dibuat = '';
+        noLkn          = ''
+        tanggal_dibuat = ''
     } else{
-        noLkn = props.lkn[0].LKN;
-        tanggal_dibuat = props.lkn[0].tgl_dibuat;
+        noLkn          = props.lkn[0].LKN
+        tanggal_dibuat = props.lkn[0].tgl_dibuat
+        noPenangkapan  = props.lkn[0].penangkapan[0].no_penangkapan
+        tglPenangkapan = props.lkn[0].penangkapan[0].tanggal_penangkapan
+        jamPenangkapan = props.lkn[0].penangkapan[0].jam_penangkapan
+
     }
+    console.log('props', props.lkn)
+
     return (
       <ViewContainer>
         <Descriptions title="LKN Info">
             <Descriptions.Item label="No. LKN">{noLkn}</Descriptions.Item>
             <Descriptions.Item label="Tanggal Dibuat">{tanggal_dibuat}</Descriptions.Item>
+        </Descriptions>
+
+        <Descriptions title='Penangkapan'>
+    <Descriptions.Item label='No. Penangkapan'>{noPenangkapan}</Descriptions.Item>
+    <Descriptions.Item label='Tanggal Penangkapan'>{tglPenangkapan}</Descriptions.Item>
+    <Descriptions.Item label='Jam Penangkapan'>{jamPenangkapan}</Descriptions.Item>
         </Descriptions>
       </ViewContainer>
     );
