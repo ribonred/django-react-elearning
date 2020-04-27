86e481ab60601b28009aa2f19fec26f95b7b71f6
agus randhani <agus.randhani@versafleet.co>
2020-04-28 01:44:53 +0700

    fix conflict

--- scholl-app/frontend/src/pages/dashboard/LKN/edit.js
+++ scholl-app/frontend/src/pages/dashboard/LKN/edit.js
@@ -79,7 +79,7 @@ class EditLkn extends Component {
 
     onsubmit = async() => {
       this.setState({ isLoading: true })
-      await this.props.dispatch(editLKN(get_token(), this.state.form, this.props.lknData[0].id))
+      await this.props.dispatch(editLKN(get_token(), this.state.form, this.props.lknData.id))
       if(!this.props.error){
         history.push('/dashboard/LKN')
       } else {
