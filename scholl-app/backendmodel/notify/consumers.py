a72f15dd0264d061545191f3af3cecfac2d4cad5
redloratech <rede.akbar@loratechai.com>
2020-06-14 18:38:13 +0700

    tes

--- scholl-app/backendmodel/notify/consumers.py
+++ scholl-app/backendmodel/notify/consumers.py
@@ -9,11 +9,8 @@ class NotificationConsumer(WebsocketConsumer):
     def connect(self):
        # Checking if the User is logged in
         if self.scope["user"].is_anonymous:
-            # Reject the connection
-            self.close()
-        else:
             # print(self.scope["user"])   # Can access logged in user details by using self.scope.user, Can only be used if AuthMiddlewareStack is used in the routing.py
-            self.group_name = str(self.scope["user"].pk)  # Setting the group name as the pk of the user primary key as it is unique to each user. The group name is used to communicate with the user.
+            self.group_name = "tes"  # Setting the group name as the pk of the user primary key as it is unique to each user. The group name is used to communicate with the user.
             async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
             self.accept()
 
