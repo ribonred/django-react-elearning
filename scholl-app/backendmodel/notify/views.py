9497ae21f5704ea4e1cbafc55f4f635195927afa
redloratech <rede.akbar@loratechai.com>
2020-08-16 01:42:17 +0700

    permision

--- scholl-app/backendmodel/notify/views.py
+++ scholl-app/backendmodel/notify/views.py
@@ -2,10 +2,13 @@ from rest_framework import serializers, generics
 from backendmodel.core.models import User
 from .models import NotificationsLkn
 from .serializers import notifserializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class ActivityView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = notifserializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user =self.request.user
